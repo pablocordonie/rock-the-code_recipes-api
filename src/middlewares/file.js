b3754518ca2c590a656fa2a80b76a93f791dcbe7
pablocordonie <pablogarcia.93.06@gmail.com>
2024-04-29 18:34:29 +0200

    feat: change the name of the folder prop in file.js

--- src/middlewares/file.js
+++ src/middlewares/file.js
@@ -5,7 +5,7 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
-        folder: 'Recipe',
+        folder: 'Food',
         allowedFormats: ['jpg', 'png', 'jpeg', 'gif', 'webp']
     }
 });
